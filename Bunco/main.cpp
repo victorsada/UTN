cd6990bcae26b71f48bb16709cfea21ca7fd23ba
Salomon Sada <sajosaca@gmail.com>
2020-10-28 15:40:16 -0300

    Creando repositorio

---
+++ Bunco/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+#include "opciones.h"
+
+int main () {
+    int opciones;
+    bool juega = true;
+    string players[2], higherScore="fulano";
+
+    menu();
+    cin>>opciones;
+
+
+    while (true) {
+        /// Escoger la opcion
+        switch(opciones) {
+            case 1:
+                system("cls");
+                onePlayer();
+            break;
+
+            case 2:
+                system("cls");
+                cout<<"Ingrese nombre del jugador 1: ";
+                cin>>players[0];
+                cout<<"Ingrese nombre del jugador 2: ";
+                cin>>players[1];
+
+                twoPlayer(players[0], players[1]);
+            break;
+
+            case 3:
+                system("cls");
+                higher(higherScore);
+            break;
+
+            case 4:
+                system("cls");
+                simular();
+            break;
+
+            case 5:
+                system("cls");
+                cout<<"Gracias por usar nuestro programa. \n\n";
+                return 0;
+            break;
+        }
+        /// Caso de ingresar un dato equivocado:
+        if (opciones < 1 || opciones > 5) {
+            cout<<"\n\n"<<"Haz escogido "<<opciones<<". Selecciona una opcion valida, por favor. \n\n";
+            system("pause");
+            system("cls");
+        }
+        /// Al terminar de ejecutar la opcion seleccionada en el men�, se muestra nuevamente:
+        menu();
+        cin>>opciones;
+    }
+
+
+    return 0;
+}
---
+++ Bunco/opciones.h
@@ -0,0 +1,46 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+void jugar() {
+
+}
+void onePlayer() {
+    string player1;
+    cout<<"Ingrese su nombre: ";
+    cin>>player1;
+    cout<<"\n"<<"Jugador 1: "<<player1<<"\n\n";
+    system("pause");
+    system("cls");
+}
+
+void twoPlayer(string player1, string player2) {
+    cout<<"\n"<<"Jugador 1: "<<player1<<" | Jugador 2: "<<player2<<"\n\n";
+    system("pause");
+    system("cls");
+}
+
+void higher(string player/**, int buncos */) {
+    cout<<"Puntaje mas alto: \n\n"<<"Nombre del jugador: "<<player<<" | Cantidad de Buncos (Esto sera una variable) \n\n";
+    system("pause");
+    system("cls");
+}
+
+void simular() {
+    cout<<"Aca jugaras en el modo simulado \n\n";
+    system("pause");
+    system("cls");
+}
+
+void menu() {
+    cout<<"::::: Bienvenido a jugar Bunco ::::: \n\n Escoja el numero de la opcion que desea seleccionar: \n\n";
+    cout<<"1) Juego nuevo para UN jugador. \n2) Juego nuevo para DOS jugadores. \n3) Puntacion mas alta. \n";
+    cout<<"4) Modo simulado (carga de datos manual). \n5) Salir. \n\n Ingresar: " ;
+}
+
+
+#endif // FUNCIONES_H_INCLUDED
